b1cda6ad8a1a3e4e3e33a6fe33e50a8d18e5d79d
haiyun.zhou <haiyun.zhou@coupang.com>
2017-08-20 20:44:59 +0900

    jkmushi-mall first commit

---
+++ front-end/mmall-fe/src/page/common/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Henry
+* @Date:   2017-05-09 16:48:55
+* @Last Modified by:   Henry
+* @Last Modified time: 2017-05-17 21:20:30
+*/
+
+'use strict';
+
+require('./layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
---
+++ front-end/mmall-fe/src/page/module.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Henry
+* @Date:   2017-05-09 16:47:03
+* @Last Modified by:   Henry
+* @Last Modified time: 2017-05-09 16:47:14
+*/
+
+'use strict';
+module.exports = {
+    test: 1234
+}
